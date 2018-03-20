18f8e103198e94b91d95121cc339e594ba0c31e0
Jonathan Cooper <jonathan.cooper@onepeloton.com>
2018-03-20 10:35:22 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ADIUM_MSG_STYLE_DIR="~/Library/Application Support/Adium 2.0/Message Styles/"
+
+if [ ! -d "${ADIUM_MSG_STYLE_DIR}" ] ; then
+    echo "no adium message style dir"
+    exit 1
+fi
+
+cp -pR old-school-aim-style "${ADIUM_MSG_STYLE_DIR}"
